8e524b95d9c7a4982b2e4698a8dbeb1ad42765e2
Shivam Gagneja <119136441+ShivamGagneja@users.noreply.github.com>
2023-09-04 21:17:09 +0530

    Update settings.py

--- Watchob/settings.py
+++ Watchob/settings.py
@@ -16,19 +16,19 @@ from . mailinfo import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Email Configurations form mailinfo
-EMAIL_USE_TLS = EMAIL_USE_TLS
-EMAIL_HOST = EMAIL_HOST
-EMAIL_HOST_USER = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-EMAIL_PORT = EMAIL_PORT
+# Email Configurations form mailinfo (** Commented out for security purpose)
+# EMAIL_USE_TLS = EMAIL_USE_TLS
+# EMAIL_HOST = EMAIL_HOST
+# EMAIL_HOST_USER = EMAIL_HOST_USER
+# EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+# EMAIL_PORT = EMAIL_PORT
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uq^)p3$i$hh$177+kvpui6fqk2rnxr1!epwxjfwy%r_nuem-58'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
